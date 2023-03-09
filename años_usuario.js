77ae3c2b9cdd7997f09dab4605aa679179077152
Dolo-B <127196342+Dolo-B@users.noreply.github.com>
2023-03-09 18:23:40 +0100

    Add files via upload

---
+++ años_usuario.js
@@ -0,0 +1,33 @@
+
+
+
+
+let año = 4
+
+if(año === 1){
+    console.log("enero");
+}else if (año === 2){
+    console.log("febrero");
+}else if(año === 3){
+    console.log("marzo")
+}else if(año === 4){
+    console.log("abril")
+}else if(año === 5){
+    console.log("mayo")
+}else if(año ===6){
+    console.log("junio")
+}else if(año === 7){
+console.log("julio")
+}else if(año === 8){
+    console.log("agosto")
+}else if(año === 9){
+    console.log("septiembre")
+    }else if(año === 10){
+        console.log("octubre")
+    }else if(año === 11){
+        console.log("noviembre")
+    }else if(año === 12){
+        console.log("diciembre")
+    }else if(año > 12){
+        console.log("El año solo tiene 12 meses, salame")
+    }
